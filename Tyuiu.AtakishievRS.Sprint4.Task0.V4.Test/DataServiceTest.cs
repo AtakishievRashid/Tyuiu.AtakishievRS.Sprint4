b5a026be74068ad53da869f50e99bbfe06bcd731
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-11-07 21:21:31 +0500

    Add project files.

---
+++ Tyuiu.AtakishievRS.Sprint4.Task0.V4.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.AtakishievRS.Sprint4.Task0.V4.Lib;
+namespace Tyuiu.AtakishievRS.Sprint4.Task0.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService service = new();
+            int[] array = new int[] { 5, 3, 7, 1, 3, 9, 8, 7, 9, 4 };
+            Assert.AreEqual(32, service.GetMultEvenArrEl(array));
+
+        }
+    }
+}
---
+++ Tyuiu.AtakishievRS.Sprint4.Task0.V4/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.AtakishievRS.Sprint4.Task0.V4.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнил: Атакишиев Р.С. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #4                                                              *");
+        Console.WriteLine("* Выполнил: Атакишиев Р. С. | РППб-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+        Console.WriteLine("* статическими значениями в диапазоне от 0 до 9 подсчитать произведение   *");
+        Console.WriteLine("* четных элементов массива.  {5 ,3 ,7 ,1 ,3 ,9 ,8 ,7 ,9 ,4}               *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Массив { 5, 3, 7, 1, 3, 9, 8, 7, 9, 4 }");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        int[] array = new int[] { 5, 3, 7, 1, 3, 9, 8, 7, 9, 4 };
+        Console.WriteLine(ds.GetMultEvenArrEl(array));
+        Console.ReadKey();
+    }
+}
