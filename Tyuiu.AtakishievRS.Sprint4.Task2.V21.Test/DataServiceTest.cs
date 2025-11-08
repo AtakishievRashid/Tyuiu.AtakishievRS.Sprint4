2c50251ff2b746770b2c13b001aabd1770ca2cc7
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-11-08 19:53:23 +0500

    Task2

---
+++ Tyuiu.AtakishievRS.Sprint4.Task2.V21.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AtakishievRS.Sprint4.Task2.V21.Lib;
+namespace Tyuiu.AtakishievRS.Sprint4.Task2.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService(); 
+            int[] array = { 2, 3, 4, 5, 6 }; 
+
+            int result = ds.Calculate(array);
+
+            Assert.AreEqual(48, result);
+        }
+    }
+}
