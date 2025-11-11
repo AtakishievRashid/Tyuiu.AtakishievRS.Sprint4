fe3765cf7da8fe64718b80dfc4d736c187c5c6f6
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-11-11 23:20:21 +0500

    Task3

---
+++ Tyuiu.AtakishievRS.Sprint4.Task3.V3.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.AtakishievRS.Sprint4.Task3.V3.Lib
+{
+    public class DataService : ISprint4Task3V3
+    {
+        public int Calculate(int[,] array)
+        {
+            int max = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                max = Math.Max(max, array[2, i]);
+            }
+            return max;
+        }
+    }
+}
---
+++ Tyuiu.AtakishievRS.Sprint4.Task3.V3.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.AtakishievRS.Sprint4.Task3.V3.Lib;
+namespace Tyuiu.AtakishievRS.Sprint4.Task3.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] array = new int[5, 5]
+            {
+                {1,2,3,4,5 },
+                {5,4,3,2,1 },
+                {7,8,9,6,5 },
+                {0,0,0,0,0 },
+                {1,2,3,4,5 }
+            };
+            int result = ds.Calculate(array);
+            Assert.AreEqual(9, result);
+
+        }
+    }
+}
