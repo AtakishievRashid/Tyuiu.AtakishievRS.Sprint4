ae50544a5da6c8dae9fba275e2ac827f414acd95
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-11-12 16:33:23 +0500

    Task5

---
+++ Tyuiu.AtakishievRS.Sprint4.Task5.V5.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.AtakishievRS.Sprint4.Task5.V5.Lib;
+namespace Tyuiu.AtakishievRS.Sprint4.Task5.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = new int[3, 3] { { 1, -5, 3 },
+                                           { 2, -5, 4 },
+                                           { 3, -5, 5 } };
+            int res = ds.Calculate(matrix);
+            int wait = 18;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
