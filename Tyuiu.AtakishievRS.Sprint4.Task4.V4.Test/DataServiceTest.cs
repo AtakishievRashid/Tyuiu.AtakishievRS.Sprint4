cd3274b5736a087673fc1b4435933ad5ad943e81
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-11-12 16:22:47 +0500

    Task4

---
+++ Tyuiu.AtakishievRS.Sprint4.Task4.V4.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AtakishievRS.Sprint4.Task4.V4.Lib;
+namespace Tyuiu.AtakishievRS.Sprint4.Task4.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            var expected = new int[,] { { 1, 1 }, { 1, 7 } };
+            var input = new int[,] { { 2, 4 }, { 8, 7 } };
+
+            CollectionAssert.AreEqual(expected, ds.Calculate(input));
+        }
+    }
+}
