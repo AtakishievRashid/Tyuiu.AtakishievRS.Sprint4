8e180e1533e4cfb8d1c122688225b3aec0d1c2f4
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-11-15 04:45:27 +0500

    Task1

--- Tyuiu.AtakishievRS.Sprint4.Task1.V14.Lib/DataService.cs
+++ Tyuiu.AtakishievRS.Sprint4.Task1.V14.Lib/DataService.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.AtakishievRS.Sprint4.Task1.V14.Lib
 
             for (int i = 0; i < array.Length; i++)
             {
-                if (array[i] % 2 == 1) 
+                if (array[i] % 2 != 0) 
                 {
                     sum += array[i]; 
                 }
