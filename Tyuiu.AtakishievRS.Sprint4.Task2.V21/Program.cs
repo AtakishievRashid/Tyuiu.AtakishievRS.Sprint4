343a541fc7fd58abd814ca5a6faf110fc8c8b51a
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-11-15 12:48:02 +0500

    Task2

--- Tyuiu.AtakishievRS.Sprint4.Task2.V21/Program.cs
+++ Tyuiu.AtakishievRS.Sprint4.Task2.V21/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using Tyuiu.AtakishievRS.Sprint4.Task2.V21.Lib;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнил: Атакишиев Р.С. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Одномернные массивы (генератор случайных чисел)                   *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #21                                                             *");
+        Console.WriteLine("* Выполнил: Атакишиев Р. С. | РППб-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан одномерный целочисленный массив на 15 элементов заполненный         *");
+        Console.WriteLine("* случайными в диапазоне от 1 до 7 подсчитать произведение четных         *");
+        Console.WriteLine("* элементов массива.                                                      *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+
+
+        int[] array = new int[15];
+        Random random = new Random();
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(1, 8);
+        }
+        for (int i = 0; i < array.Length; i++) 
+        {
+            Console.Write(array[i] + " ");
+        }
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int res = ds.Calculate(array);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
